7f7a6823cbb22a9b96a49675541f9f9770f65530
James Calligeros <jcalligeros99@gmail.com>
2024-04-01 18:45:25 +1000

    install: use dist-kernel instead of sources
    This simplifies the process of getting set up
    
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- install.sh
+++ install.sh
@@ -39,62 +39,13 @@ install_m1n1() {
 }
 
 
-merge_kernel_sources() {
-        # Install a package.use for the kernel
-        if [[ ! -d /etc/portage/package.use ]]; then
-                mkdir -p /etc/portage/package.use
-        fi
-        echo "sys-kernel/asahi-sources symlink" >> /etc/portage/package.use/kernel
-        emerge -qv asahi-sources
-        echo "The patched kernel sources are now available in"
-        echo "/usr/src/linux."
-}
-
-
-make_kernel() {
-        echo "We are going to install a known-good kernel for you now. You"
-        echo "can edit this at any time after the install procedure has finished."
-        echo "In fact, you should edit it once you've booted in to the filesystem."
+install_distkernel() {
+        echo "We will now install the Asahi dist-kernel for you."
         echo
         read -sp "Press Enter to continue..."
         echo
-        # Check if genkernel is installed
-        if [[ ! -f /usr/bin/dracut ]]; then
-                emerge -qv dracut
-        fi
-        echo "sys-apps/kmod zstd" >> /etc/portage/package.use/kernel
-        emerge -qv kmod
-        zcat /proc/config.gz > /usr/src/linux/.config
 
-        make -C /usr/src/linux -j $(nproc)
-        KERNVER=$(make -C /usr/src/linux -s kernelrelease)
-
-        make -C /usr/src/linux modules_install
-        # Gentoo's GRUB expects Image and the initramfs to be
-        # in pairs with the same release tag, with the tag
-        # -linux taking precedence over all others. If
-        # a kernel already exists with that tag, we need to
-        # move it so that our kernel and initramfs become the
-        # default booted
-        if [[ -e /boot/vmlinu{x,z}-linux ]]; then
-                mv /boot/vmlinu{x,z}-linux /boot/vmlinu{x,z}-old
-        fi
-        if [[ -e /boot/initramfs-linux.img ]]; then
-                mv /boot/initramfs-linux.img /boot/initramfs-old.img
-        fi
-        make -C /usr/src/linux install
-
-        if [[ ! -d /etc/dracut.conf.d/ ]]; then
-                mkdir -p /etc/dracut.conf.d/
-        fi
-
-        cp resources/dracut.conf /etc/dracut.conf.d/10-apple.conf
-
-        dracut \
-                --force \
-                --quiet \
-                --kver ${KERNVER} \
-                --compress gzip
+        emerge -q sys-kernel/asahi-kernel virtual/dist-kernel
 
         # We need to rebuild GRUB
         grub-install --removable --efi-directory=/boot --boot-directory=/boot
@@ -154,9 +105,7 @@ install_uboot
 
 install_grub
 
-merge_kernel_sources
-
-make_kernel
+install_distkernel
 
 install_m1n1
 
