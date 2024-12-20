c56a276d87404bb2aee9233cc5af090cd6d59d35
James Calligeros <jcalligeros99@gmail.com>
2024-12-20 11:06:11 +1000

    install.sh: drop virtual/rust
    Replaced with new slotted rust behaviour.
    
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- install.sh
+++ install.sh
@@ -40,7 +40,6 @@ install_meta() {
 	echo "VIDEO_CARDS=\"asahi\"" >> /etc/portage/make.conf
 
         emerge -q1 dev-lang/rust-bin
-        emerge -q virtual/rust
         emerge -q sys-apps/asahi-meta virtual/dist-kernel sys-kernel/linux-firmware
         asahi-fwupdate
         update-m1n1
